971665a4d5291b57f6c63a00408468f3ab506bf7
xeho91 <xeho91@pm.me>
2023-01-08 21:23:15 +0100

    chore(lint-staged): 🔧  Use shared config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,23 +1,8 @@
+import { CONFIG } from "@terminal-nerds/lint-staged-config";
+
 // https://github.com/okonet/lint-staged
-
 const config = {
-	// Prettier
-	"*": ["pretty-quick --check --staged"],
-
-	// ESLint
-	"*.{js,json,ts,yml,yaml}": ["eslint"],
-
-	// depcheck & syncpack
-	"**/package.json": () => [
-		"depcheck",
-		"syncpack list-mismatches",
-		"syncpack format",
-	],
-
-	// markdownlint
-	"*.md": [`markdownlint --ignore "./.changeset/*.md"`],
-
-	"*.ts": ["tsc --noEmit"],
+	...CONFIG,
 };
 
 export default config;
