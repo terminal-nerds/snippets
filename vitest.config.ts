25fb4e05dfe9e1f4d592f21b5a1f6d0ce789d4c1
xeho91 <xeho91@pm.me>
2023-01-08 01:41:41 +0100

    chore(Vitest): 🔧  Reuse shared config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,7 @@
+import { WITH_COVERAGE_OPTIONS } from "@terminal-nerds/vitest-config";
 import { defineConfig } from "vitest/config";
 
+// https://vitest.dev/config/
 export default defineConfig({
-	test: {
-		open: false,
-		ui: true,
-	},
+	test: { ...WITH_COVERAGE_OPTIONS },
 });
