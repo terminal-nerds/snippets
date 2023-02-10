8b2b2f045e68578664e92a05a2dae50b78915c88
xeho91 <xeho91@pm.me>
2023-02-10 16:06:13 +0800

    test(is-string): 🚨  Resolve duplication warning

--- packages/string/source/is-string/is-string.test.ts
+++ packages/string/source/is-string/is-string.test.ts
@@ -4,13 +4,13 @@ import { NON_STRING_VALUES, STRING_VALUES } from "../../tests/shared.js";
 import { isString } from "./is-string.js";
 
 describe("isString(value)", () => {
-	it("returns `true` for string values", () => {
+	it("returns `true` for passed string parameters", () => {
 		for (const string of STRING_VALUES) {
 			expect(isString(string)).toBe(true);
 		}
 	});
 
-	it("returns `false` for non-string values", () => {
+	it("returns `false` for passed non-string parameters", () => {
 		for (const nonString of NON_STRING_VALUES) {
 			expect(isString(nonString)).toBe(false);
 		}
