6e24887d9f10a82b54cc3e36b363319bb6759c2f
xeho91 <xeho91@pm.me>
2023-02-10 15:43:14 +0800

    feat(string): ✨  Add `isStringEmpty()` with tests

--- packages/string/source/main.ts
+++ packages/string/source/main.ts
@@ -1,3 +1,4 @@
 export * from "./is-string/is-string.js";
+export * from "./is-string-empty/is-string-empty.js";
 export * from "./schema/schema.js";
 export * from "./validate-string/validate-string.js";
