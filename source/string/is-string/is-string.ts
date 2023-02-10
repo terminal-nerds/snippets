f7ef5cf5b455a8b143c56300d29856a745563bd4
xeho91 <xeho91@pm.me>
2023-02-10 12:44:27 +0800

    chore: 🏗  Remove `source/` directory

--- source/string/is-string/is-string.ts
+++
@@ -1,5 +0,0 @@
-import { STRING_SCHEMA } from "../string.schema.js";
-
-export function isString(value: unknown): value is string {
-	return STRING_SCHEMA.safeParse(value).success;
-}
--- source/string/main.ts
+++
@@ -1,4 +0,0 @@
-export * from "./is-string/is-string.js";
-export * from "./is-string-empty/is-string-empty.js";
-export * from "./string.schema.js";
-export * from "./validate-string/validate-string.js";
