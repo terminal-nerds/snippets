f46b0fde89053cf6ab9437af7b47a18712bab2e2
xeho91 <xeho91@pm.me>
2023-03-14 16:06:25 +0800

    test(runtime): ✅  Temporarily disable testing `edge-runtime`

--- packages/runtime/source/environment/environment.edge.test.ts
+++ packages/runtime/source/environment/environment.edge.test.ts
@@ -1,77 +1,79 @@
-import { returns } from "@terminal-nerds/snippets-test/unit";
+/* @vitest-environment edge-runtime */
+
+// FIXME: Something is wrong with zod getting instanceof cls.name, when environment is edge-runtime
+// import { returns } from "@terminal-nerds/snippets-test/unit";
 import { describe, expect, it } from "vitest";
 
-import {
-	getRuntimeEnvironmentName,
-	IN_BROWSER,
-	IN_BUN,
-	IN_DENO,
-	IN_DOM,
-	IN_EDGE,
-	IN_HAPPY_DOM,
-	IN_JSDOM,
-	IN_NODE,
-	IN_WEB_WORKER,
-} from "./environment.js";
+// import {
+// 	getRuntimeEnvironmentName,
+// 	IN_BROWSER,
+// 	IN_BUN,
+// 	IN_DENO,
+// 	IN_DOM,
+// 	IN_EDGE,
+// 	IN_HAPPY_DOM,
+// 	IN_JSDOM,
+// 	IN_NODE,
+// 	IN_WEB_WORKER,
+// } from "./environment.js";
 
-// @vitest-environment edge-runtime
-describe("getRuntimeEnvironmentName() - in Edge Runtime", () => {
-	it(returns("edge"), () => {
-		expect(getRuntimeEnvironmentName()).toBe("edge");
-	});
+// describe.todo("getRuntimeEnvironmentName() - in Edge Runtime", () => {
+// 	it.fails(returns("edge"), () => {
+// 		expect(getRuntimeEnvironmentName()).toBe("edge");
+// 	});
+// });
+
+describe.todo("IN_BROWSER", () => {
+	// it.fails(returns(false), () => {
+	// 	expect(IN_BROWSER).toBe(false);
+	// });
 });
 
-describe("IN_BROWSER", () => {
-	it(returns(false), () => {
-		expect(IN_BROWSER).toBe(false);
-	});
+describe.todo("IN_BUN", () => {
+	// it.fails(returns(false), () => {
+	// 	expect(IN_BUN).toBe(false);
+	// });
 });
 
-describe("IN_BUN", () => {
-	it(returns(false), () => {
-		expect(IN_BUN).toBe(false);
-	});
+describe.todo("IN_DENO", () => {
+	// it.fails(returns(false), () => {
+	// 	expect(IN_DENO).toBe(false);
+	// });
 });
 
-describe("IN_DENO", () => {
-	it(returns(false), () => {
-		expect(IN_DENO).toBe(false);
-	});
+describe.todo("IN_DOM", () => {
+	// it.fails(returns(false), () => {
+	// 	expect(IN_DOM).toBe(false);
+	// });
 });
 
-describe("IN_DOM", () => {
-	it(returns(false), () => {
-		expect(IN_DOM).toBe(false);
-	});
+describe.todo("IN_EDGE_RUNTIME", () => {
+	// it.fails(returns(true), () => {
+	// 	expect(IN_EDGE).toBe(true);
+	// });
 });
 
-describe("IN_EDGE_RUNTIME", () => {
-	it(returns(true), () => {
-		expect(IN_EDGE).toBe(true);
-	});
+describe.todo("IN_HAPPY_DOM", () => {
+	// it.fails(returns(false), () => {
+	// 	expect(IN_HAPPY_DOM).toBe(false);
+	// });
 });
 
-describe("IN_HAPPY_DOM", () => {
-	it(returns(false), () => {
-		expect(IN_HAPPY_DOM).toBe(false);
-	});
+describe.todo("IN_JSDOM", () => {
+	// it.fails(returns(false), () => {
+	// 	expect(IN_JSDOM).toBe(false);
+	// });
 });
 
-describe("IN_JSDOM", () => {
-	it(returns(false), () => {
-		expect(IN_JSDOM).toBe(false);
-	});
-});
-
-describe("IN_NODE", () => {
+describe.todo("IN_NODE", () => {
 	// NOTE: This test is being run in Node.js.
-	it.fails(returns(false), () => {
-		expect(IN_NODE).toBe(false);
-	});
+	// it.fails(returns(false), () => {
+	// 	expect(IN_NODE).toBe(false);
+	// });
 });
 
-describe("IN_WEB_WORKER", () => {
-	it(returns(false), () => {
-		expect(IN_WEB_WORKER).toBe(false);
-	});
+describe.todo("IN_WEB_WORKER", () => {
+	// it.fails(returns(false), () => {
+	// 	expect(IN_WEB_WORKER).toBe(false);
+	// });
 });
