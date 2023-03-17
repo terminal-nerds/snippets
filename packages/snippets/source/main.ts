cef09dece00451326b88df62436ab13f684c90b2
xeho91 <xeho91@pm.me>
2023-03-17 14:25:12 +0800

    fix(snippets): 🐛  Missing packages

--- packages/snippets/source/main.ts
+++ packages/snippets/source/main.ts
@@ -2,6 +2,7 @@
 export * from "@terminal-nerds/snippets-config";
 export * from "@terminal-nerds/snippets-error";
 export * from "@terminal-nerds/snippets-extension";
+export * from "@terminal-nerds/snippets-function";
 export * from "@terminal-nerds/snippets-regexp";
 export * from "@terminal-nerds/snippets-runtime";
 export * from "@terminal-nerds/snippets-string";
