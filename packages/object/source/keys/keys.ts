3d9d33875fae89ec5027ebf1223612f0ec2662ad
xeho91 <xeho91@pm.me>
2023-04-06 14:43:39 +0800

    feat(object): ✨  Improve `renamer` parameter

--- packages/object/source/keys/keys.ts
+++ packages/object/source/keys/keys.ts
@@ -1,18 +1,28 @@
 import { z } from "zod";
 
+import { validateObject } from "../schema/schema.ts";
+
 export const OBJECT_KEY_SCHEMA = z.number().or(z.string()).or(z.symbol());
 
-const RENAMER_SCHEMA = z.function().args(OBJECT_KEY_SCHEMA).returns(OBJECT_KEY_SCHEMA);
+const RENAMER_SCHEMA = z.function().args(OBJECT_KEY_SCHEMA, z.number().optional()).returns(OBJECT_KEY_SCHEMA);
 
 export type ObjectKey = number | string | symbol;
 
 export function renameObjectKeys<OldKey extends ObjectKey, Value, NewKey extends ObjectKey>(
 	object: Record<OldKey, Value>,
-	renamer: (key: OldKey) => NewKey,
+	renamer: (key: OldKey, index: number) => NewKey,
 ): Record<NewKey, Value> {
+	validateObject(object);
 	RENAMER_SCHEMA.parse(renamer);
 
+	function handleRename(key: OldKey, index: number) {
+		return OBJECT_KEY_SCHEMA.parse(renamer(key, index));
+	}
+
 	return Object.fromEntries(
-		(Object.entries(object) as Array<[OldKey, Value]>).map(([key, value]) => [renamer(key), value]),
+		(Object.entries(object) as Array<[OldKey, Value]>).map(([key, value], index) => [
+			handleRename(key, index),
+			value,
+		]),
 	) as Record<NewKey, Value>;
 }
