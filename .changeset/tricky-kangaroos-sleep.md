05015d567ddfc7beff51b9c09aac59c06f3380b9
Mateusz Kadlubowski <xeho91@pm.me>
2023-05-06 13:48:16 +0800

    fix(runtime): 🐛  Remove another occurence of using `RuntimeError` (#91)

---
+++ .changeset/tricky-kangaroos-sleep.md
@@ -0,0 +1,5 @@
+---
+"@terminal-nerds/snippets-runtime": patch
+---
+
+🐛 Removed `RuntimeError` usage due to CJS issues with `modern-erro`
--- packages/runtime/source/environment/environment.ts
+++ packages/runtime/source/environment/environment.ts
@@ -1,4 +1,4 @@
-import { RuntimeError } from "@terminal-nerds/snippets-error/custom";
+/* import { RuntimeError } from "@terminal-nerds/snippets-error/custom"; */
 import { z } from "zod";
 /* prettier-ignore */
 export const RUNTIME_ENVIRONMENTS = [
@@ -15,43 +15,65 @@ export const RUNTIME_ENVIRONMENTS = [
 export type RuntimeEnvironment = (typeof RUNTIME_ENVIRONMENTS)[number];
 export const RUNTIME_ENVIRONMENT_SCHEMA = z.enum(RUNTIME_ENVIRONMENTS);
 
-/** @see {@link https://bun.sh/docs/api/globals} */
+/**
+ * @see {@link https://bun.sh/docs/api/globals}
+ */
 export const IN_BUN = globalThis.Bun !== undefined && Bun.version !== undefined;
 
-/** @see {@link https://deno.land/manual@v1.31.1/runtime#deno-global} */
+/**
+ * @see {@link https://deno.land/manual@v1.31.1/runtime#deno-global}
+ */
 export const IN_DENO = globalThis.Deno !== undefined && Deno.version !== undefined && Deno.version.deno !== undefined;
 
-/** Detects if the DOM is available. */
+/**
+ * Detects if the DOM is available.
+ */
 export const IN_DOM =
 	globalThis.window !== undefined &&
 	!globalThis.navigator !== undefined &&
 	// @ts-ignore FIXME: Find a way to make it typed?
 	window.document !== undefined;
 
-/** @see {@link https://edge-runtime.vercel.app/features/available-apis#addressing-the-runtime} */
+/**
+ * @see {@link https://edge-runtime.vercel.app/features/available-apis#addressing-the-runtime}
+ */
 // @ts-ignore FIXME: Find a way to make it typed?
 export const IN_EDGE = globalThis.EdgeRuntime !== undefined;
 
-/** @see {@link https://github.com/jsdom/jsdom} */
+/**
+ * @see {@link https://github.com/jsdom/jsdom}
+ */
 export const IN_JSDOM = IN_DOM && navigator.userAgent.includes("jsdom");
 
-/** @see {@link https://github.com/capricorn86/happy-dom/discussions/481} */
+/**
+ * @see {@link https://github.com/capricorn86/happy-dom/discussions/481}
+ */
 // @ts-ignore FIXME: Find a way to make it typed?
 export const IN_HAPPY_DOM = IN_DOM && window.happyDOM !== undefined;
 
-/** @see {@link https://nodejs.org/dist/latest-v18.x/docs/api/process.html#process} */
+/**
+ * @see {@link https://nodejs.org/dist/latest-v18.x/docs/api/process.html#process}
+ */
 export const IN_NODE = globalThis.process !== undefined && process.version !== undefined;
 
-/** Detect if is running in browser, and is not in a browser emulator (e.g. jsdom or Happy DOM) */
+/**
+ * Detect if is running in browser, and is not in a browser emulator (e.g. jsdom or Happy DOM)
+ */
 export const IN_BROWSER = IN_DOM && !IN_HAPPY_DOM && !IN_JSDOM;
 
-/** @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers} */
+/**
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers}
+ */
 export const IN_WEB_WORKER =
 	typeof self === "object" && self.constructor && self.constructor.name === "DedicatedWorkerGlobalScope";
 
-/** Receive the name _(in lowercase)_ of the currently running JavaScript environment. */
+/**
+ * Receive the name _(in lowercase)_ of the currently running JavaScript environment.
+ */
 export function getRuntimeEnvironmentName(): RuntimeEnvironment {
-	/** NOTE: Order matters! */
+	/**
+	 * NOTE: Order matters!
+	 */
 	const conditions = {
 		"edge": IN_EDGE,
 		"bun": IN_BUN,
@@ -68,7 +90,7 @@ export function getRuntimeEnvironmentName(): RuntimeEnvironment {
 		if (isTrue) return name as RuntimeEnvironment;
 	}
 
-	throw new RuntimeError(`Unrecognized JavaScript runtime environment!`);
+	throw new Error(`Unrecognized JavaScript runtime environment!`);
 }
 
 export function validateRuntimeEnvironmentName(name: string): asserts name is RuntimeEnvironment {
