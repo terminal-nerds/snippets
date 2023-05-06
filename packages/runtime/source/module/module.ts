c96212933965c88879a87b79df65358aa1cf0733
xeho91 <xeho91@pm.me>
2023-05-06 11:39:34 +0800

    fix(runtime/module): 🐛  Use a native `Error` instead of custom `RuntimeError`
    Temporary workaround for issues with CJS from `modern-error`

--- packages/runtime/source/module/module.ts
+++ packages/runtime/source/module/module.ts
@@ -1,16 +1,21 @@
-import { RuntimeError } from "@terminal-nerds/snippets-error/custom";
+/* import { RuntimeError } from "@terminal-nerds/snippets-error/custom"; */
 import { resolveModule } from "local-pkg";
 
 import { IN_BROWSER } from "../environment/environment.ts";
 
-/** @see {@link https://nodejs.org/api/modules.html#modules-commonjs-modules} CommonJS Module */
+/**
+ * @see {@link https://nodejs.org/api/modules.html#modules-commonjs-modules} CommonJS Module
+ */
 export const IN_CJS = typeof globalThis.require === "function";
 
-/** @see {@link https://nodejs.org/api/esm.html#modules-ecmascript-modules} ECMAScript Module */
+/**
+ * @see {@link https://nodejs.org/api/esm.html#modules-ecmascript-modules} ECMAScript Module
+ */
 export const IN_ESM = !IN_CJS && Boolean(import.meta);
 
 export function hasModule(name: string): boolean {
-	if (IN_BROWSER) throw new RuntimeError(`You cannot check for module existence in the browser.`);
+	/* if (IN_BROWSER) throw new RuntimeError(`You cannot check for module existence in the browser.`); */
+	if (IN_BROWSER) throw new Error(`You cannot check for module existence in the browser.`);
 
 	return Boolean(resolveModule(name));
 }
